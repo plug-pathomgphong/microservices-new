6977fea5320ea7867d965432da4676bd7d7aa57a
plug <plug185@gmail.com>
2023-03-27 13:32:20 +0700

    [Add] Notification Service with kafka

--- order-service/src/main/java/com/pathomphong/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/pathomphong/orderservice/service/OrderService.java
@@ -3,12 +3,14 @@ package com.pathomphong.orderservice.service;
 import com.pathomphong.orderservice.dto.InventoryResponse;
 import com.pathomphong.orderservice.dto.OrdeLineItemsDto;
 import com.pathomphong.orderservice.dto.OrderRequest;
+import com.pathomphong.orderservice.event.OrderPlacedEvent;
 import com.pathomphong.orderservice.model.Order;
 import com.pathomphong.orderservice.model.OrderLineItems;
 import com.pathomphong.orderservice.repository.OrderRepository;
 import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -27,6 +29,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final Tracer tracer;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
     
     public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -60,6 +63,7 @@ public class OrderService {
 
                 if (allProductsInStock){
                     orderRepository.save(order);
+                    kafkaTemplate.send("notificationTopic",new OrderPlacedEvent(order.getOrderNumber()));
                     return "Order Placed Successfully";
                 }else {
                     throw new IllegalArgumentException("Product is not in stock, please try again later");
